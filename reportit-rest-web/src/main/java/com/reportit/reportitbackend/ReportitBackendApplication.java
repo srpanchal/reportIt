224673f7bab15972f09a8c3d7ca39883b68d1c16
saurabh <saurabh.panchal@gdn-commerce.com>
2019-03-08 21:55:42 +0530

    initial apis

--- reportit-rest-web/src/main/java/com/reportit/reportitbackend/ReportitBackendApplication.java
+++ reportit-rest-web/src/main/java/com/reportit/reportitbackend/ReportitBackendApplication.java
@@ -2,8 +2,9 @@ package com.reportit.reportitbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ReportitBackendApplication {
 
 	public static void main(String[] args) {
