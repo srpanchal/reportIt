7f0e9792fb98582c20383e86b666a107d70d4985
Sarang Sharma <sarang.sharma@coviam.com>
2019-03-08 23:43:47 +0530

    user endpoint

--- reportit-config/src/main/java/com/reportit/reportitbackend/SecurityConfig.java
+++ reportit-config/src/main/java/com/reportit/reportitbackend/SecurityConfig.java
@@ -4,9 +4,14 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @Configuration
 @EnableOAuth2Sso
+@RestController
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] AUTH_WHITELIST = {
@@ -27,5 +32,11 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated().and().logout().logoutSuccessUrl("/").permitAll();
         http.cors().and().csrf().disable();
     }
+
+  @RequestMapping("/user")
+  public Principal user(Principal principal) {
+    return principal;
+  }
+
 }
 
