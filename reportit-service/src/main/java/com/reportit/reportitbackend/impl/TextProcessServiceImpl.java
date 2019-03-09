7d38b1debe0172c5cdafb4246c90c9ab80cdb93b
Jahnavi Suthar <jahnavi.suthar@coviam.com>
2019-03-10 03:44:25 +0530

    depndency changes

--- reportit-service/src/main/java/com/reportit/reportitbackend/impl/TextProcessServiceImpl.java
+++ reportit-service/src/main/java/com/reportit/reportitbackend/impl/TextProcessServiceImpl.java
@@ -1,23 +1,23 @@
-//package com.reportit.reportitbackend.impl;
-//
-//import com.filter.textcorrector.TextFilter;
-//import com.filter.textcorrector.spellchecking.Language;
-//
-//import java.util.List;
-//
-//public class TextProcessServiceImpl {
-//
-//    private TextFilter textFilter = new TextFilter(Language.ENGLISH);
-//
-//    public List<String> getWordSuggestions(String input) {
-//        return textFilter.checkWord(input);
-//    }
-//
-//    public boolean checkForProfanity(String word) {
-//        List<String> compundWordList = textFilter.checkCompound(word);
-//        return textFilter.isProfane(word) || (!compundWordList.isEmpty() && textFilter
-//            .isProfane(compundWordList.get(0)));
-//    }
-//
-//    //    String text = textFilter.preproccess("p13ce of sh1t", false);
-//}
+package com.reportit.reportitbackend.impl;
+
+import com.filter.textcorrector.TextFilter;
+import com.filter.textcorrector.spellchecking.Language;
+
+import java.util.List;
+
+public class TextProcessServiceImpl {
+
+    private TextFilter textFilter = new TextFilter(Language.ENGLISH);
+
+    public List<String> getWordSuggestions(String input) {
+        return textFilter.checkWord(input);
+    }
+
+    public boolean checkForProfanity(String word) {
+        List<String> compundWordList = textFilter.checkCompound(word);
+        return textFilter.isProfane(word) || (!compundWordList.isEmpty() && textFilter
+            .isProfane(compundWordList.get(0)));
+    }
+
+    //    String text = textFilter.preproccess("p13ce of sh1t", false);
+}
