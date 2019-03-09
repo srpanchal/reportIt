5e9bbe469e58b82288bc83299075cf3f3499e38e
Jahnavi Suthar <jahnavi.suthar@coviam.com>
2019-03-10 00:33:37 +0530

    Commented textProcessingCode

--- reportit-service/src/main/java/com/reportit/reportitbackend/TextProcessService.java
+++ reportit-service/src/main/java/com/reportit/reportitbackend/TextProcessService.java
@@ -1,8 +1,8 @@
-package com.reportit.reportitbackend;
-
-import java.util.List;
-
-public interface TextProcessService {
-    List<String> getWordSuggestions(String input);
-    boolean checkForProfanity(String word);
-}
+//package com.reportit.reportitbackend;
+//
+//import java.util.List;
+//
+//public interface TextProcessService {
+//    List<String> getWordSuggestions(String input);
+//    boolean checkForProfanity(String word);
+//}
