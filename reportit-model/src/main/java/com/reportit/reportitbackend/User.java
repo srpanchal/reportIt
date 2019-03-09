a3734247efc671b5794c3f4614f5cec4dfecabf9
Jahnavi Suthar <jahnavi.suthar@coviam.com>
2019-03-09 14:23:09 +0530

    Fix Id

--- reportit-model/src/main/java/com/reportit/reportitbackend/User.java
+++ reportit-model/src/main/java/com/reportit/reportitbackend/User.java
@@ -3,11 +3,7 @@ package com.reportit.reportitbackend;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.annotation.Version;
-import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
